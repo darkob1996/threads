11fe635ad818e251c27ea938bfeecc42a3c1a4f4
Darko Boljanović <71256137+darkob1996@users.noreply.github.com>
2024-10-21 20:49:04 +0200

    ovo vise nema smisla

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -4,6 +4,13 @@ import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+import {
+  addMemberToCommunity,
+  createCommunity,
+  deleteCommunity,
+  removeUserFromCommunity,
+  updateCommunityInfo,
+} from "@/lib/actions/community.action";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
