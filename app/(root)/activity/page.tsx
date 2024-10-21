dff8ef19c598f0a5dee271beea14d97c81213860
Darko Boljanović <71256137+darkob1996@users.noreply.github.com>
2024-10-21 19:16:29 +0200

    redeploy again

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -19,7 +19,7 @@ async function Page() {
     <section>
       <h1 className="head-text mb-10">Activity</h1>
 
-      <section className="mt-10 flex flex-col gap-5">
+      <section className="mt-10 flex flex-col gap-6">
         {activity && activity.length > 0 ? (
           <>
             {activity.map((activity) => (
